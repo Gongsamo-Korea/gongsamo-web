8dafc4fbf1404df51c35d89a0bfaa2dc093af461
somangoi <somi.c.hwang@gmail.com>
2023-04-09 10:05:17 +0900

    initial-settings: add global style to layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,27 @@
+import AppScripts from '@/components/AppScripts/AppScripts';
+import { GlobalStyle } from '@/styles';
+import Head from 'next/head';
+import React from 'react';
+
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"
+        />
+      </Head>
+      <AppScripts />
+      <GlobalStyle />
+
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
