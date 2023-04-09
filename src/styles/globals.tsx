b68852a7c9a7a884c19220a53b828d7087d4668c
somangoi <somi.c.hwang@gmail.com>
2023-04-09 10:04:54 +0900

    initial-settings: add font.css

--- src/styles/globals.tsx
+++ src/styles/globals.tsx
@@ -81,8 +81,8 @@ const style = css`
   }
 
   body {
-    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
-      Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+    font-family: 'Pretendard', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
       'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
   }
 
