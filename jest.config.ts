146fa3e863dc1a9f33c04b13b88275cf3bd8ecde
neokkk <seven3126@gmail.com>
2023-05-13 21:54:01 +0900

    chore: 설정 정리 및 의존성 추가
    - axios, msw, react-query

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,24 @@
-import { Config } from 'jest';
+import nextJest from 'next/jest';
 import { name as appName } from './package.json';
 
-const config: Config =  {
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config = {
   clearMocks: true,
   displayName: appName,
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  preset: 'ts-jest',
   setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.ts?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   verbose: true,
 };
 
-export default config;
+export default createJestConfig(config);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
-import React from 'react';
 import Layout from '@/components/common/layouts/Layout';
+import { queryClient } from '@/services/query';
 import '@/styles/colors.css';
 import '@/styles/effects.css';
 import '@/styles/fonts.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
