9c7f3be6cbda1d8318e789f2bf205443cd2f0da3
Somi Hwang <somi.c.hwang@gmail.com>
2023-05-28 10:54:22 +0900

    add: mock data

---
+++ src/contents/about.ts
@@ -0,0 +1,29 @@
+export const MORE_INFO = [
+  {
+    subtitle: '공사모를 소개합니다',
+    title: 'What is this?',
+    contents:
+      'This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.',
+    date: '2023/5/5',
+    tags: ['tag1', 'tag2', 'tag3'],
+    link: 'https://www.google.com',
+  },
+  {
+    subtitle: '멤버 인터뷰',
+    title: 'What is this?2',
+    contents:
+      'This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.',
+    date: '2023/5/5',
+    tags: ['tag1', 'tag2', 'tag3'],
+    link: 'https://www.google.com',
+  },
+  {
+    subtitle: '헬로',
+    title: 'What is this?3',
+    contents:
+      'This is a newsletter app that allows you to create, edit, and delete newsletters. It also allows you to add tags to your newsletters and search for newsletters by tags.',
+    date: '2023/5/5',
+    tags: ['tag1', 'tag2', 'tag3'],
+    link: 'https://www.google.com',
+  },
+];
