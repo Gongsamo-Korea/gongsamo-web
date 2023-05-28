18ed5e43cf614a87d555bd79773a90df83dc0732
Somi Hwang <somi.c.hwang@gmail.com>
2023-05-28 10:54:43 +0900

    add: content card

---
+++ src/components/ui/cards/ContentCard.tsx
@@ -0,0 +1,141 @@
+import styled from '@emotion/styled';
+import { ContentCardProps } from '@/types/props';
+import Typography15 from '../textStyles/Typography15';
+import Typography13 from '../textStyles/Typography13';
+import Typography11 from '../textStyles/Typography11';
+import { useState } from 'react';
+import Image from 'next/image';
+import theme from '@/styles/theme';
+
+const ContentCard = ({
+  backgroundColor,
+  color,
+  tags,
+  title,
+  subtitle,
+  date,
+  author,
+  contents,
+}: ContentCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Wrapper
+      backgroundColor={backgroundColor ?? theme.colors.blue1}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <TitleWrapper>
+        {subtitle && (
+          <Typography13 text={subtitle} color={color ?? theme.colors.gray9} weight={400} />
+        )}
+        <Typography15 text={title} color={color ?? theme.colors.gray9} weight={700} />
+        {date && <Typography11 text={date} color={color ?? theme.colors.gray9} weight={400} />}
+      </TitleWrapper>
+      {isHovered ? (
+        <InfoWrapper>
+          {author && (
+            <Typography11 text={author} color={color ?? theme.colors.gray9} weight={400} />
+          )}
+          {contents && (
+            <Typography11 text={contents} color={color ?? theme.colors.gray9} weight={400} />
+          )}
+        </InfoWrapper>
+      ) : (
+        <>
+          <ThumbnailWrapper>
+            <Image
+              src="/images/contents_thumbnail.jpg"
+              alt={'thumbnail'}
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: '100%', height: 'auto' }}
+            />
+            <TagWrapper>
+              {tags &&
+                tags.map((tag, index) => (
+                  <Tag key={index} index={index}>
+                    <Typography11 text={tag} color={theme.colors.gray9} weight={400} />
+                  </Tag>
+                ))}
+            </TagWrapper>
+          </ThumbnailWrapper>
+        </>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div<{ backgroundColor: string }>`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+
+  padding: 0px;
+  width: 37rem;
+  height: 24rem;
+
+  flex: none;
+  flex-grow: 0;
+
+  background: ${({ backgroundColor }) => backgroundColor};
+  border: 1px solid ${({ theme }) => theme.colors.gray8};
+`;
+
+const TitleWrapper = styled.div`
+  flex: 1;
+  padding: 1.6rem;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  gap: 0.4rem;
+  height: 100%;
+  padding: 0 1.6rem;
+  margin-bottom: 1.6rem;
+  overflow-y: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+`;
+
+const ThumbnailWrapper = styled.div`
+  overflow-y: hidden;
+  width: 100%;
+  height: 14rem;
+  position: relative;
+
+  img {
+    min-width: 100%;
+  }
+`;
+
+const TagWrapper = styled.div`
+  position: absolute;
+  bottom: 1.4rem;
+  right: 1.6rem;
+  display: flex;
+  gap: 0.8rem;
+`;
+
+const Tag = styled.div<{ index: number }>`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.4rem 1.2rem;
+
+  width: 5rem;
+  height: 2rem;
+
+  background: ${({ index, theme }) =>
+    [theme.colors.yellow1, theme.colors.blue1, theme.colors.green1][index]};
+
+  border: 1px solid ${({ theme }) => theme.colors.gray9};
+  border-radius: 16px;
+
+  flex: none;
+  flex-grow: 0;
+`;
+
+export default ContentCard;
--- src/types/props.ts
+++ src/types/props.ts
@@ -1,5 +1,20 @@
+import { ColorType } from '@/styles/theme';
+import { Theme } from '@emotion/react';
+
 export interface IconProps {
   width?: number;
   height?: number;
   color?: string;
+}
+
+export interface ContentCardProps {
+  backgroundColor?: string;
+  color?: string;
+  tags?: string[];
+  title: string;
+  subtitle?: string;
+  date?: string;
+  author?: string;
+  contents?: string;
+  link?: string;
 }
