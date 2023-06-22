052e600d6e764b274cc5680666c25fbd971c6d80
ctaaag <qudxor102@gmail.com>
2023-06-23 02:20:19 +0900

    ✨ADD: newletter 페이지 getServerSideProps 적용

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,17 +1,24 @@
 import Link from 'next/link';
-import SearchNewsletter from '@/components/SearchNewsletter/SearchNewsletter';
-import Categories from '@/components/Category/Categories';
-import { fetchNewsletters } from '@/apis/newsletter';
+import styled from '@emotion/styled';
+import { useEffect } from 'react';
+
 import { useNewslettersStore } from '@/stores/newsletters';
 
-const Newsletter = () => {
-  const { newsletters, query, page, totalPages, setNextPage } = useNewslettersStore();
+import SearchNewsletter from '@/components/SearchNewsletter/SearchNewsletter';
+import Categories from '@/components/Category/Categories';
+
+const Newsletter = ({ articles, page, totalPages }: any) => {
+  const { newsletters, query, setNextPage } = useNewslettersStore();
   const hasNextPage = page < totalPages;
 
-  const handleFetchNext = async () => {
-    const nextNewsletters = await fetchNewsletters({ query, page: page + 1 });
-    setNextPage(nextNewsletters.results);
-  };
+  // const handleFetchNext = async () => {
+  //   const nextNewsletters = await fetchNewsletters({ query, page: page + 1 });
+  //   setNextPage(nextNewsletters.results);
+  // };
+
+  useEffect(() => {
+    useNewslettersStore.getState().setNewsletters(articles, page, totalPages);
+  }, [articles]);
 
   return (
     <>
@@ -37,10 +44,32 @@ const Newsletter = () => {
             </li>
           ))}
         </ol>
-        {hasNextPage && <button onClick={handleFetchNext}>더보기</button>}
+        {/* {hasNextPage && <button onClick={handleFetchNext}>더보기</button>} */}
       </section>
     </>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const { keyword = '', page = 1, size = 9 } = context.query;
+
+  const res = await fetch(
+    `http://localhost:3000/api/articles?keyword=${keyword}&page=${page}&size=${size}`,
+    {
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+  );
+  const { results, totalPages } = await res.json();
+  useNewslettersStore.getState().setNewsletters(results, page, totalPages);
+  return {
+    props: {
+      articles: useNewslettersStore.getState().newsletters,
+      totalPages: useNewslettersStore.getState().totalPages,
+      page: useNewslettersStore.getState().page,
+    },
+  };
+}
 
 export default Newsletter;
