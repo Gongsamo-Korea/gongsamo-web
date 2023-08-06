fc2ba9bbdd11481d506cb5803f5899af89ef1c78
ctaaag <qudxor102@gmail.com>
2023-08-06 19:33:38 +0900

    🛠FIX : 페이지네이션 수정, 카테고리 볼드 처리, 김칩 문구 및 이미지 수정

--- src/components/Category/CategoryTag.tsx
+++ src/components/Category/CategoryTag.tsx
@@ -6,9 +6,11 @@ import type { Category } from '@/models/category';
 const CategoryTag = ({
   category,
   onClick,
+  selectedCategory,
 }: {
   category: Category;
   onClick: (category: any) => void;
+  selectedCategory: any;
 }) => {
   return (
     <Wrapper
@@ -17,23 +19,34 @@ const CategoryTag = ({
           Math.floor(Math.random() * 4)
         ]
       }
+      borderWidth={selectedCategory == category.id ? '2px' : '1px'}
       onClick={() => onClick(category.id)}
     >
-      <Typography15 text={category.name} color={theme.colors.gray9} weight={400} />
+      <Typography15
+        text={category.name}
+        color={theme.colors.gray9}
+        weight={selectedCategory == category.id ? 700 : 400}
+      />
     </Wrapper>
   );
 };
 
-const Wrapper = styled.button<{ background: string }>`
+const Wrapper = styled.button<{ background: string; borderWidth: string }>`
+  &:hover {
+    border: 0.2rem solid ${({ theme }) => theme.colors.gray9};
+    p:first-of-type {
+      font-weight: 600;
+    }
+  }
   display: inline-flex;
   align-items: center;
 
   box-sizing: border-box;
-  padding: 0.2rem 1rem;
+  padding: 0.1rem 1rem;
 
   border-radius: 1.5rem;
   border: 0.1rem solid ${({ theme }) => theme.colors.gray8};
-
+  border-width: ${({ borderWidth }) => borderWidth};
   background: ${({ background }) => background};
 `;
 
