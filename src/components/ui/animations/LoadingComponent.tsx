31b590b077fee2aba0d05c91115bdb9f46b0ba65
Somi Hwang <somi.c.hwang@gmail.com>
2023-08-06 22:15:08 +0900

    ADD: loading component on newsletter page

---
+++ src/components/ui/animations/LoadingComponent.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from './lottie/loading.json';
+
+const animationOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+interface LoadingComponentProps {
+  width?: number;
+  height?: number;
+}
+
+const LoadingComponent = ({ width = 80, height = 80 }) => {
+  return <Lottie options={animationOptions} width={width} height={height} />;
+};
+
+export default LoadingComponent;
